c2364fc40b2a23d274a93035c3838b98ac73bc4b
Your Name <you@example.com>
2023-12-14 20:57:44 +0100

    insert

---
+++ insert.sql
@@ -0,0 +1,12 @@
+use edunovawp3;
+select * from smjerovi;
+
+insert into smjerovi (naziv,trajanje,cijena,vaucer)
+values ('Web programiranje', 225 , 1859.45, 1);
+
+
+insert into smjerovi (naziv,trajanje,cijena,vaucer)
+values ('Web programiranje', 225 , 1859.46, 1);
+
+insert into smjerovi (naziv)
+values ('žćđšĐŠĆČ');
